9565796d4e84dc6aad9150e74629f25a74f2598c
Jason <jason.dixon.email@gmail.com>
2015-04-20 11:18:18 +1200

    improving builder

--- builder.py
+++ builder.py
@@ -4,12 +4,12 @@ import os
 path = os.path.dirname(os.path.realpath(__file__))
 
 src = os.path.join(path, "__init__.py")
+src = os.path.join(path, "test.py")
 dest = os.path.join(path, "build.mel")
 
 with open(src, "r") as f:
     data = f.read()
 
-data = "print \"HELLO\""
 
 with open(dest, "w") as f:
     f.write("""// Simple Script Installer. Created 20/04/2015 Jason Dixon.
@@ -27,5 +27,6 @@ $user = "internetimagery"; // Owner of repo
     f.write("python(\"")
     data = data.replace("\\", "\\\\")
     data = data.replace("\"", "\\\"")
+    data = data.replace("\n", r"\n")
     f.write(data)
     f.write("\");")
