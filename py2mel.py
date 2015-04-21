4b03a0655db6cb3e5139911d88f4678b924e1fbe
Jason <jason.dixon.email@gmail.com>
2015-04-22 01:55:00 +1200

    updated builder

--- py2mel.py
+++ py2mel.py
@@ -17,20 +17,36 @@ import argparse
 import datetime
 import sys
 
-version = "v1.0.0"
 parser = argparse.ArgumentParser(
     description="Convert Python file to Melscript (using python interpreter).",
     epilog="Use either standard in and out, the -input -output flags or a combination of both. ie: cat INPUTFILE | py2mel.py > OUTPUTFILE")
 parser.add_argument("-i", "--input", help="Input file for processing.", type=argparse.FileType('r'))
 parser.add_argument("-o", "--output", help="Output file for processing.", type=argparse.FileType('w'))
+parser.add_argument("-s", "--shelf", help="Optional! Name to give to shelf icon if dropping causes shelf icon.", type=str)
 args = parser.parse_args()
 
 
-def parse_python(data, version=False):
-    output = "// py2mel.py %s, compiled %s\n\n" % (version, datetime.datetime.today()) if version else ""
-    output += "python(\"%s\");" % data.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", r"\n")
+def stringify(data):
+    return data.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", r"\n")
+
+
+def version():
+    version = "v1.0.1"
+    return "py2mel.py %s, compiled %s\n\n" % (version, datetime.datetime.today())
+
+
+def python_interpret(data):
+    output = "// %s" % version()
+    output += "python(\"%s\");" % stringify(data)
     return output
+
+
+def python_shelf(data, name):
+    code = "# %s" % version()
+    code += data
+    return "shelfButton -l \"%s\" -c \"%s\" -stp \"python\" -i \"daisyLarge.png\" -p `tabLayout -query -selectTab $gShelfTopLevel`;" % (name, stringify(code))
 
 inp = args.input if args.input else sys.stdin
 out = args.output if args.output else sys.stdout
-out.write(parse_python(inp.read(), version))
+data = python_shelf(inp.read(), args.shelf) if args.shelf else python_interpret(inp.read())
+out.write(data)
